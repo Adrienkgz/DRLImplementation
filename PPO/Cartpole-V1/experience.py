416ecd597ec11df9f02d9af108158bdfd0951f40
Adrienkgz <adrien.kegreisz@icloud.com>
2025-02-09 22:58:08 +0100

    maj ppo

--- PPO/Cartpole-V1/experience.py
+++ PPO/Cartpole-V1/experience.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 import numpy as np
-
+from collections import deque
 @dataclass
 class Experience:
     state: np.ndarray
     action: int
     reward: float
     next_state: np.ndarray
-    advantage: float
+    logprobs: float
+    done: bool
+    td_error: float = 0.0
+    advantage: float = 0.0
+    
--- PPO/Cartpole-V1/ppo.py
+++ PPO/Cartpole-V1/ppo.py
@@ -6,6 +6,7 @@ import gymnasium as gym
 from collections import deque
 from custom_wrapper import create_custom_cartpole
 from experience import Experience
+from keras.optimizers import Adam
 
 # Parameters
 GAMMA = 0.99
@@ -17,9 +18,10 @@ LR_ACTOR = 3e-4
 LR_CRITIC = 1e-3
 
 class PPOTF:
-    def __init__(self, env: gym.Env, verbose: int = 0):
+    def __init__(self, env: gym.Env, batch_size, verbose: int = 0):
         # Initialization of personal variables
         self.verbose = verbose
+        self.batch_size = batch_size
         
         # Initialization of algorithm variables
         self.T = T
@@ -35,7 +37,11 @@ class PPOTF:
         # Policy and value networks
         self.actor = self.create_actor()
         self.critic = self.create_critic()
-    
+        self.optimizer = Adam()
+        
+        if self.T % self.batch_size != 0:
+            raise ValueError('Batch size must me a multiple of T')
+        
     def create_actor(self):
         # Creation of the actor network
         actor = tf.keras.Sequential([
@@ -57,6 +63,16 @@ class PPOTF:
         critic.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LR_CRITIC), loss='mse')
         return critic
     
+    def get_action_and_value(self, x, action=None):
+        probs = self.actor(x)
+        print(probs)
+        if action is None:
+            action = probs.sample()
+        return action, probs[action]
+    
+    def get_value(self, x):
+        return self.critic(x)
+        
     def train(self, episodes: int = 1000):
         for episode in range(episodes):
             state = env.reset()
@@ -64,11 +80,11 @@ class PPOTF:
             done = False
             
             while not done:
-                action = self.get_action(state)
+                action, logprobs = self.get_action_and_value(state)
                 next_state, reward, done, _, _ = self.env.step(action)
                 
                 # I have decided to use a class Experience to make the code easier to understand
-                self.buffer.append(Experience(state, action, reward, next_state, 0))
+                self.buffer.append(Experience(state, action, reward, next_state, logprobs, done))
                 
                 if self.actual_step % T == 0:
                     self.update()
@@ -78,15 +94,67 @@ class PPOTF:
     def update(self):
         for _ in range(self.epochs):
             self.compute_advantages()
+            self.update_actor_policy()
+            self.update_critic()
             
-    def compute_advantages(self):
-        for experience in self.buffer:
-            # Formule : δ = r + lambda (V(s_t+1) - V(s_t))
+    def compute_advantages(self):   
+        # We traverse the buffer in reverse to calculate the advantage
+        last_advantage = 0
+        for experience in self.buffer.reverse():
+            # Compute td_error
+            # Formule : δ = r + gamma * V(s_t+1) - V(s_t)
+            # => δ = r + gamma * V(s_t+1) - V(s_t) if done = false
+            # => δ = r - V(s_t) if done = True
+            experience.td_error = experience.reward + self.gamma * self.get_value(experience.next_state) * (1 - experience.done) - self.get_value(experience.state)
             
-            delta_t = experience.reward + self.lambda_gae * (experience.next_state - experience.state)
+            #Compute advantage
+            if experience.done:
+                last_advantage = 0
             
-            experience.advantage =
-                
+            experience.advantage = experience.td_error + self.gamma * self.lambda_gae * last_advantage
+            
+    def update_actor_policy(self):
+        def compute_clip_loss(batch):
+            old_policies = np.array([experience.logprobs for experience in batch])
+            _, new_policies = self.get_action_and_value([experience.state for experience in batch])
+            advantages = np.array([experience.advantages for experience in batch])
+            
+            ratio_policy = new_policies / old_policies
+            
+            unclipped_loss_policy = ratio_policy * advantages
+            
+            clipped_loss_policy = tf.clip_by_value(ratio_policy, 1 - self.epsilon_clip, 1 + self.epsilon_clip) * advantages
+            
+            loss_policy = - tf.minimum(unclipped_loss_policy, clipped_loss_policy).mean()
+            
+            return loss_policy
+            
+        number_of_batches = self.T // self.batch_size
+        # We update by batch to go faster
+        for i in range(0, number_of_batches):
+            #Possiblement enlever -1 au in range
+            batch = self.buffer[i*self.batch_size:(i+1)*self.batch_size]
+
+            # Update the actor
+            loss = compute_clip_loss(batch)
+            self.optimizer.minimize(lambda: loss, var_list=self.critic.trainable_variables)
+
+            
+    def update_critic(self):
+        def compute_loss_critic(batch):
+            states = np.array([experience.state for experience in batch])
+            
+            return 
+        
+        number_of_batches = self.T // self.batch_size
+        # We update by batch to go faster
+        for i in range(0, number_of_batches):
+            #Possiblement enlever -1 au in range
+            batch = self.buffer[i*self.batch_size:(i+1)*self.batch_size]
+
+            # Update the critic
+            loss = compute_loss_critic(batch)
+            self.optimizer.minimize(lambda: loss, var_list=self.critic.trainable_variables)
             
                 
                 
